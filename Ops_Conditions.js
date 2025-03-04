5927044f7147456436bb4b0af9c94c951b5f4ef1
Aboli-SB <aboli.more@securityboat.net>
2025-03-04 12:51:45 +0000

    Learned about Assignment Operator

---
+++ Ops_Conditions.js
@@ -0,0 +1,23 @@
+//Arithmetic Operators, Comparison Operators (== vs ===), Logical Operators, Assignment Operators, Bitwise Operators
+//Arithmetic
+let x = 5;
+let y = 2;
+let z = x + y;
+let z1 = x - y;
+let z2 = x * y;
+let z3 = x / y;
+let z4 = x % y;
+x++;
+let z5 = x;
+x--;
+let z6 = x;
+let x1 = 5;
+let z7 = x ** 2;
+
+//Comparison operator
+
+//Logical Operator
+
+//Assignment Operator
+
+
