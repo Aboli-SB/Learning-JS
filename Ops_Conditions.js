3ee69c3ba88f97d61b9e6426ef40b888938b1616
Aboli-SB <aboli.more@securityboat.net>
2025-03-05 05:26:25 +0000

    Studied Conditional operation.

--- Ops_Conditions.js
+++ Ops_Conditions.js
@@ -2,17 +2,14 @@
 //Arithmetic
 let x = 5;
 let y = 2;
-let z = x + y;
-let z1 = x - y;
-let z2 = x * y;
-let z3 = x / y;
-let z4 = x % y;
-x++;
-let z5 = x;
-x--;
-let z6 = x;
-let x1 = 5;
-let z7 = x ** 2;
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(++x);
+console.log(x--);
+console.log(x ** 2);
 
 //Comparison operator
 let a = 10, b = 20;
@@ -26,17 +23,17 @@ console.log("Less Than (<):", a < b);
 console.log("Less Than or Equal (<=):", a <= b);
 
 //Logical Operator
-let a1 = true, b1 = false, x1 = 5, y1 = 10;
+let a1 = true, b1 = false, x11 = 5, y1 = 10;
 
 console.log("AND:", a1 && b1); 
 console.log("OR:", a1 || b1); 
 console.log("NOT:", !a1); 
 
 //Bitwise Operator
-console.log("Bitwise AND:", x1 & y1); 
-console.log("Bitwise OR:", x1 | y1); 
-console.log("Bitwise XOR:", x1 ^ y1);
-console.log("Bitwise NOT:", ~x1); 
+console.log("Bitwise AND:", x11 & y1); 
+console.log("Bitwise OR:", x11 | y1); 
+console.log("Bitwise XOR:", x11 ^ y1);
+console.log("Bitwise NOT:", ~x11); 
 
 //Assignment Operator
 let num = 10;  
@@ -80,3 +77,41 @@ console.log("After num >>= 1:", num);
 
 num >>>= 1;  
 console.log("After num >>>= 1:", num);  
+
+//if-else
+if (num > 10) {
+    console.log("Number is greater than 10");
+} else if (num === 10) {
+    console.log("Number is exactly 10");
+} else {
+    console.log("Number is less than 10");
+}
+
+//switch
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    default:
+        console.log("Weekend or Invalid Day");
+}
+
+//ternary Operator
+let age = 18;
+
+let result = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+console.log(result);
